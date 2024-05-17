89f45227ef6cbb3c9b84bd50cfc18aa8478a6fa9
Foxglovery <foxglovery@gmail.com>
2024-05-17 13:10:37 -0500

    fleshed out navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
+import contactImg from "../../assets/contact.png";
 // REFCE!!!!! USe it for shortcut function
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src="" alt="" />
-      <div className="desktopMenu"></div>
+      <img src={logo} alt="logo" className="logo" />
+      <div className="desktopMenu">
+        <Link to="/#" className="desktopMenuListItem">
+          Home
+        </Link>
+        <Link to="/#" className="desktopMenuListItem">
+          About
+        </Link>
+        <Link to="/#" className="desktopMenuListItem">
+          Portfolio
+        </Link>
+        <Link to="/#" className="desktopMenuListItem">
+          Clients
+        </Link>
+      </div>
       <button className="desktopMenuBtn">
-        <img className="desktopMenuImg" src="" alt="" />
+        <img className="desktopMenuImg" src={contactImg} alt="" />
         Contact Me
       </button>
     </nav>
