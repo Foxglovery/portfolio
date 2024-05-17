512c16e1bfd81edcbc9f37d374d66783701675c0
Foxglovery <foxglovery@gmail.com>
2024-05-17 11:16:43 -0500

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+// REFCE!!!!! USe it for shortcut function
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src="" alt="" />
+      <div className="desktopMenu"></div>
+      <button className="desktopMenuBtn">
+        <img className="desktopMenuImg" src="" alt="" />
+        Contact Me
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
