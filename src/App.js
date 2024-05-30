4bbdb886328c5ea339a81e8f0b50b9059d85e519
Foxglovery <foxglovery@gmail.com>
2024-05-30 10:25:16 -0500

    added Portfolio section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
 import Skills from "./components/Skills/Skills";
+import Works from "./components/Works/Works";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
       <Skills />
+      <Works />
     </div>
   );
 }
---
+++ src/components/Works/Works.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Works.css";
+
+const Works = () => {
+  return <section id="works">Works Done</section>;
+};
+
+export default Works;
